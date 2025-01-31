f063b37bb434715df82ca31313987c3aab7de628
Roselaine Moreira <roselaine.moreira@safeweb.com.br>
2025-01-31 16:50:20 -0300

    Ajust layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 
 import LogoAT from "../../assets/img/logo-auto-travel.png";
-import "./style.css";
+import IUser from "../../assets/icons/user.png";
+import IMaps from "../../assets/icons/pin.png";
+
 import Footer from "../../components/Footer";
 import Passageiros from "../Passageiros";
+
+import "./style.css";
+
 
 const Home = () => {
 
@@ -25,22 +30,76 @@ const Home = () => {
                         />
                     </div>
                 </div>
-                <div className="col-5 mt-5">
-                    <button className="btn btn-light btn-lg"
-                        onClick={() => setPassageiro(true)}><b>Passageiros</b></button>
-                </div>
+                {/* <div className="col-5 mt-5">
+                    
+                </div> */}
             </header>
-            
+
             {
-                passageiro ? <Passageiros/> :
+                passageiro ? <Passageiros /> :
                     <>
                         <div className="text-center bg-home">
                             {/* Home principal */}
-                            <div className="row" style={{ width: "50%" }}>
-                                <div className="col-12 text-home" style={{ marginTop: "200px" }}>
-                                    <h1><b>AutoTravel! <br />Simplificando Processos das<br />
-                                        listas de Passageiros!</b></h1>
+                            <div className="row justify-content-evenly mt-5" style={{ background: "rgb(255 255 255 / 24%)", height: "70vh", width: "96%", margin: "0 auto" }}>
+                                {/* Card Passageiros */}
+                                <div className="col-12 col-md-4 col-lg-3 mt-2 ms-1" onClick={() => setPassageiro(true)}>
+                                    <div className="card shadow-lg d-flex flex-row align-items-center p-3"
+                                        style={{
+                                            // width: "300px",
+                                            height: "200px",
+                                            border: "2px solid #1c4986",
+                                            borderRadius: "7px",
+                                            transition: "all 0.3s ease-in-out",
+                                            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)"
+                                        }}
+                                        onMouseEnter={(e) => {
+                                            e.currentTarget.style.boxShadow = "0px 10px 20px rgba(0, 0, 0, 0.2)";
+                                            e.currentTarget.style.transform = "translateY(-5px)";
+                                        }}
+                                        onMouseLeave={(e) => {
+                                            e.currentTarget.style.boxShadow = "0px 4px 8px rgba(0, 0, 0, 0.1)";
+                                            e.currentTarget.style.transform = "translateY(0px)";
+                                        }}
+                                    >
+                                        <img src={IUser} className="img-fluid" alt="Imagem do Card" style={{ width: "32px", height: "32px", objectFit: "contain" }} />
+                                        <div className="card-body d-flex align-items-center" style={{ width: "100%", height: "100%" }}>
+                                            <h4 className="card-title m-0"><b>Passageiros</b></h4>
+                                        </div>
+                                    </div>
                                 </div>
+
+                                {/* Card Google Maps */}
+                                <div className="col-12 col-md-4 col-lg-3 mt-2 mx-1">
+                                    <a href="https://www.google.com.br/maps/" target="_blanck" rel="noopener noreferrer" 
+                                    style={{ textDecoration:"none"}}>
+
+                                        <div className="card shadow-lg d-flex flex-row align-items-center p-3"
+                                            style={{
+                                                height: "200px",
+                                                border: "2px solid #1c4986",
+                                                borderRadius: "7px",
+                                                transition: "all 0.3s ease-in-out",
+                                                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)"
+                                            }}
+                                            onMouseEnter={(e) => {
+                                                e.currentTarget.style.boxShadow = "0px 10px 20px rgba(0, 0, 0, 0.2)";
+                                                e.currentTarget.style.transform = "translateY(-5px)";
+                                            }}
+                                            onMouseLeave={(e) => {
+                                                e.currentTarget.style.boxShadow = "0px 4px 8px rgba(0, 0, 0, 0.1)";
+                                                e.currentTarget.style.transform = "translateY(0px)";
+                                            }}
+                                        >
+                                            <img src={IMaps} className="img-fluid" alt="Imagem do Card" style={{ width: "32px", height: "32px", objectFit: "contain" }} />
+                                            <div className="card-body d-flex align-items-center" style={{ width: "100%", height: "100%" }}>
+                                                <h4 className="card-title m-0"><b>Google Maps</b></h4>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div className="col-12 col-md-3 col-lg-2"></div>
+                                <div className="col-12 col-md-3 col-lg-2"></div>
                             </div>
                         </div>
                         <Footer />
